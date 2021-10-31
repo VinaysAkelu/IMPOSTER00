24cc5c6ac00954d6e8275fa02e4450f2bc7d8245
VinaysAkelu <71651242+VinaysAkelu@users.noreply.github.com>
2021-10-31 23:55:40 +0530

    Create TRAIL2.java
    To initialize a class

---
+++ TRAIL2.java
@@ -0,0 +1,43 @@
+public class Helloworld{
+  public static void main(String args[]){
+    System.out.println("Hello user");
+
+    Student s1 = new Student(1,"Rajesh",3);
+    Student s2 = new Student();
+    System.out.println("Student s1 = " + s1);
+    System.out.println("Student s2 = " + s2);
+
+    s2 = s1;
+    System.out.println("Student s1 = " + s1);
+    System.out.println("Student s2 = " + s2);
+    System.out.println("Student s1 = " + s1.getyear());
+    System.out.println("Student s1 = " + s1.getname());
+  }
+}
+
+public class Student{
+  private int id;
+  private String name;
+  private int year;
+
+  public Student(int id1, String name1, int year1)
+  {
+    id = id1;
+    name = name1;
+    year = year1;
+  }
+
+  public Student()
+  {
+  }
+
+  public String getname()
+  {
+    return name;
+  }
+
+  public int getyear()
+  {
+    return year;
+  }
+}
