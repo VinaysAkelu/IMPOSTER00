562e5677c8114510e764ee9c615795c1058ef587
VinaysAkelu <71651242+VinaysAkelu@users.noreply.github.com>
2021-11-08 00:43:08 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,4 +1,6 @@
-public class Sample{
+/*
+ * //To print diff messages for diff year admissions using students class
+ * public class Sample{
   public static void main(String args[]){
     System.out.println("Hello user ");
     students s1 = new students(2015, "CSE", 183);
@@ -7,5 +9,18 @@ public class Sample{
     System.out.println("Unique ID for admissison from or after 2015 - " + id1);
     String id2 = s2.getunique_id();
     System.out.println("Unique ID for admissison before 2015 - " + id2);
+  }
+}
+*/
+
+public class Sample{
+  public static void main(String args[]){
+    System.out.println("Hello user ");
+    CseStudent s1 = new CseStudent("Vinay", 183, 20, 2019, 5, "Flytekart training prog");
+    EceStudent s2 = new EceStudent("Topper", 106, 20, 2019, 5, 9.9f);
+    s1.getInfo();
+    s1.findGraduatingYear("Not completed");
+    s2.getInfo();
+    s2.getAge("Female");
   }
 }
